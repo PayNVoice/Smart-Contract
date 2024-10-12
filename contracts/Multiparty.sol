3914642a2cea58a896db3e47218386de414a071a
KingAkande <kingakande@gmail.com>
2024-10-12 12:09:17 +0100

    updated

--- contracts/Multiparty.sol
+++ contracts/Multiparty.sol
@@ -6,39 +6,28 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Multiparty {
 
-  enum Milestone1 {
-    COMPLETED
-  }
-
-  enum Milestone2{
-    STARTED,
-    COMPLETED
-  }
-
-  enum Milestone3 {
+  enum Milestones {
     STARTED,
     PARTLY_READY,
     COMPLETED
   }
 
 
- 
-  
-
   struct MultipartyRecord {
     address[] partyMembers;
     uint256[] totalAmountAllocatedForEachPartyMember;
-    uint256[] milestoneCountForEachPartyMember;
     uint256[] expectedDeliveryDate;
     string termsForConductingBusiness;
     uint256 penaltyRateForDefaulters;
+
   }
   mapping(address => mapping(uint256 => MultipartyRecord)) public multipartyList;
   mapping(address => uint256) public totalMultiPartySystemCreated;
   
 
   address multipartyCreator;
-  address lskTokenAddress;
+  address tokenAddress;
+  uint256 mileStoneCovered;
 
   error ADDRESS_ZERO_NOT_PERMITED();
   error NOT_AUTHORIZE_TO_CALL_THIS_FUNCTION();
@@ -55,18 +44,17 @@ contract Multiparty {
   event  DepositSuccessful(address indexed depositor , uint256 amount, uint256 counterID);
 
 
-  constructor(address _lskTokenAddress){
+  constructor(address _tokenAddress){
     if(msg.sender == address(0)){
       revert ADDRESS_ZERO_NOT_PERMITED();
     }
     multipartyCreator = msg.sender;
-     lskTokenAddress = _lskTokenAddress;
+     tokenAddress = _tokenAddress;
   }
 
   function createMultiPartySystem(
     address[] memory _partMem, 
     uint256[] memory _totalAmountForEach,
-    uint256[] memory _mileStoneCountForEach,
     uint256[] memory _expectedDateForEach,
     string memory _termsForConductingBusiness,
     uint256 _penalty
@@ -84,7 +72,7 @@ contract Multiparty {
       }
 
       if(lengthOfArrayCheck != _totalAmountForEach.length ||
-      lengthOfArrayCheck != _mileStoneCountForEach.length ||
+      //lengthOfArrayCheck != _mileStoneCountForEach.length ||
       lengthOfArrayCheck != _expectedDateForEach.length){
         
         revert LENGTH_MUST_BE_SAME_WITH_PARTYMEMBERS_LENGTH();
@@ -102,7 +90,7 @@ contract Multiparty {
       MultipartyRecord memory newRecord = MultipartyRecord({
         partyMembers: _partMem,
         totalAmountAllocatedForEachPartyMember: _totalAmountForEach,
-        milestoneCountForEachPartyMember: _mileStoneCountForEach,
+        //milestoneCountForEachPartyMember: _mileStoneCountForEach,
         expectedDeliveryDate: _expectedDateForEach,
         termsForConductingBusiness: _termsForConductingBusiness,
         penaltyRateForDefaulters: _penalty
@@ -122,10 +110,10 @@ contract Multiparty {
 
   function depositToPlatform(uint256 _amount, uint256 _counterID) public {
 
-  
+  require(msg.sender != address(0), "zero address");
     MultipartyRecord storage record = multipartyList[msg.sender][_counterID];
 
-    bool isMember = false;
+    bool isMember;
     uint256 allocatedAmount;
     
    
@@ -145,9 +133,9 @@ contract Multiparty {
         revert NOT_AN_ALLOCATED_AMOUNT();
     }
 
-    IERC20 lskToken = IERC20(lskTokenAddress); 
+    IERC20 token = IERC20(tokenAddress); 
 
-    uint256 _userTokenBalance = lskToken.balanceOf(msg.sender);
+    uint256 _userTokenBalance = token.balanceOf(msg.sender);
 
      if (_userTokenBalance < _amount) {
 
@@ -155,7 +143,9 @@ contract Multiparty {
     }
    
 
-    lskToken.transferFrom(msg.sender, address(this), _amount);
+    token.transferFrom(msg.sender, address(this), _amount);
+
+  
 
     emit  DepositSuccessful(msg.sender, _amount, _counterID);
 }
