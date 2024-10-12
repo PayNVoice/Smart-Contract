aac7a2c4ca334a74fb5a7298406b44e4a1adf08f
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-10-12 03:43:15 +0300

    updated master contract

--- contracts/master.sol
+++ contracts/master.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import './token.sol';
 
 contract MasterContract {
 
     uint256 public escrowAmount;
     uint256 penaltyRate;
     uint256 aggreementCounter;
-    address paymentToken;
+    Token public  paymentToken;
     
     struct BusinessAccount{
         string businessName;
@@ -44,50 +45,52 @@ contract MasterContract {
     mapping (address => BusinessAccount) public registeredBusiness;
     mapping(uint256 =>  B2BAgreement) public agreements;
 
-    event BusinnesRegistered(address indexed businessAddress, string indexed businessName);
+    event BusinessRegistered(address indexed businessAddress, string indexed businessName);
     event AgreementCreated(uint256 agreementId, address supplier, address customer, uint256 totalAmount);
     event DepositReceived(uint256 amount, address from);
+    event PaymentReleased(uint256 agreementId, uint256 milestoneIndex, uint256 amount);
+    event InvoiceGenerated(uint256 agreementId, uint256 milestoneIndex, string invoiceDetails);
 
     constructor(address tokenAddress){
         aggreementCounter = 1;
-        paymentToken =  tokenAddress;
+        paymentToken = Token(tokenAddress);
     }
 
-    function registerBusiness(string _name, string _category) external {
-        registerBusiness[msg.sender] = BusinessAccount({
+    function registerBusiness(string memory _name, string  memory _category) external {
+        registeredBusiness[msg.sender] = BusinessAccount({
             businessName: _name,
             category: _category,
             isRegistered: true
         });
 
-        emit BusinnesRegistered(msg.sender, businessName);
+        emit BusinessRegistered(msg.sender, _name);
     }
 
-    function createAgreement(address _supplier, address _customer, uint256 _totalAmount, string[] memory _milestoneDescription,  uint256 _deadline, uint256[] memory _milestoneAmounts, string memory _terms) external returns(uint256 aggreementId) {
+    function createAgreement(address _supplier, address _customer, uint256 _totalAmount, string[] memory _milestoneDescriptions,  uint256 _deadline, uint256[] memory _milestoneAmounts, string memory _terms) external returns(uint256 aggreementId) {
         require(_supplier != address(0) && _customer != address(0), "Invalid addresses");
 
-        B2BAgreement memory newAggreement = agreements[agreementCounter]
+        B2BAgreement storage newAggreement = agreements[aggreementCounter];
         newAggreement.supplier = _supplier;
         newAggreement.customer = _customer;
         newAggreement.totalAmount = _totalAmount;
         newAggreement.deadline = _deadline;
-        newAggreement.terms = _terms;
+        newAggreement.termsOfBusiness = _terms;
 
-        for(uint256 i = 0; i < _milestoneDescription.length; i++){
-            newAggreement.milestones.push({
-                description: _milestoneDescription[i],
+          for (uint256 i = 0; i < _milestoneDescriptions.length; i++) {
+            newAggreement.milestones.push(Milestone({
+                description: _milestoneDescriptions[i],
                 amount: _milestoneAmounts[i],
-                status: MilestoneStatus.Pending
-            })
+                status: mileStoneStatus.Pending // initial status is Pending
+            }));
         }
 
-        emit AgreementCreated(agreementCounter, _supplier, _customer, _totalAmount);
+        emit AgreementCreated(aggreementCounter, _supplier, _customer, _totalAmount);
 
-        return agreementCounter++;
+        return aggreementCounter++;
         
     }
 
-    function depositToEscrow(uint256 _aggreementId, uint256 _milestoneIndex) external {
+    function depositToEscrow(uint256 _aggreementId, uint256 amount) external {
         require(amount > 0, "Must send some funds");
         require(agreements[_aggreementId].customer == msg.sender, "Only customer can send");
 
@@ -96,9 +99,26 @@ contract MasterContract {
         emit DepositReceived(amount, msg.sender);
     }
 
+     // Release payment for a completed milestone using ERC20 tokens
+    function releasePayment(uint256 _agreementId, uint256 _milestoneIndex) internal {
+        B2BAgreement storage agreement = agreements[_agreementId];
+        require(agreement.milestones[_milestoneIndex].status == mileStoneStatus.Completed, "Milestone not completed");
+        require(agreement.milestones[_milestoneIndex].status == mileStoneStatus.customerConfirmed, "Milestone not confirmed by customer");
+        require(agreement.milestones[_milestoneIndex].status == mileStoneStatus.supplierConfirmed, "Milestone not confirmed by supplier");
+
+        uint256 paymentAmount = agreement.milestones[_milestoneIndex].amount;
+        
+        // Transfer the amount from this contract to the supplier
+        require(paymentToken.transfer(agreement.supplier, paymentAmount), "Token transfer failed");
+        
+        agreement.milestones[_milestoneIndex].status = mileStoneStatus.Paid; // Update the milestone to paid status
+
+        emit PaymentReleased(_agreementId, _milestoneIndex, paymentAmount);
+    }
+
     //invoice for a milestone
     function generateInvoice(uint256 __aggreementId, uint256 _milestoneIndex) external {
-          B2BAgreement storage agreement = agreements[_agreementId];
+          B2BAgreement storage agreement = agreements[__aggreementId];
         require(msg.sender == agreement.supplier || msg.sender == agreement.customer, "Unauthorized access");
 
         string memory invoiceDetails = string(abi.encodePacked(
@@ -108,7 +128,28 @@ contract MasterContract {
             uint2str(agreement.milestones[_milestoneIndex].amount)
         ));
 
-        emit InvoiceGenerated(_agreementId, _milestoneIndex, invoiceDetails);
+        emit InvoiceGenerated(__aggreementId, _milestoneIndex, invoiceDetails);
+    }
+
+       // Helper function to convert uint to string
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        j = _i;
+        while (j != 0) {
+            bstr[--k] = bytes1(uint8(48 + j % 10));
+            j /= 10;
+        }
+        return string(bstr);
     }
 
     
