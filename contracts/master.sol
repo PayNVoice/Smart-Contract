1fc85520111dc4064c7f852fea72e32c1fbcfcab
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-10-14 16:38:18 +0300

    updated suppliers

--- contracts/master.sol
+++ contracts/master.sol
@@ -18,6 +18,12 @@ contract MasterContract {
         bool isRegistered;
     }
 
+    struct Supplier{
+        address supplierAddresses;
+        Milestone[] milestones;
+        bool isPaid;
+    }
+
     enum mileStoneStatus{
         Pending,
         Completed,
@@ -34,27 +40,36 @@ contract MasterContract {
     }
 
     struct B2BAgreement{
-        address supplier;
+        // address supplier;
         address customer;
         uint256 totalAmount;
         uint256 deadline;
         string termsOfBusiness;
-        Milestone[] milestones;
+        address[] supplierAddresses;
+        mapping(address => Supplier) suppliers;
         bool isCompleted;
     }
+
+    
 
     mapping (address => BusinessAccount) public registeredBusiness;
     mapping(uint256 =>  B2BAgreement) public agreements;
 
     event BusinessRegistered(address indexed businessAddress, string indexed businessName);
-    event AgreementCreated(uint256 indexed agreementId, address indexed supplier, address indexed customer, uint256  totalAmount);
+    event AgreementCreated(uint256 indexed agreementId, address indexed customer, uint256 indexed  totalAmount);
     event DepositReceived(uint256 indexed amount, address from);
-    event PaymentReleased(uint256 indexed agreementId, uint256 indexed milestoneIndex, uint256 indexed amount);
-    event InvoiceGenerated(uint256 indexed agreementId, uint256 indexed milestoneIndex, string indexed invoiceDetails);
+    event PaymentReleased(uint256 indexed agreementId, address _supplier, uint256 indexed milestoneIndex, uint256 indexed amount);
+    event InvoiceGenerated(uint256 indexed agreementId, address _supplier, uint256 indexed milestoneIndex, string indexed invoiceDetails);
     event MilestoneCompleted(uint256 indexed agreementId, uint256 indexed milestoneIndex);
+    event SupplierAdded(uint256 indexed agreementId, address indexed supplier);
 
     modifier onlyRegistered(){
         require(registeredBusiness[msg.sender].isRegistered, "Business not registered");
+        _;
+    }
+
+    modifier onlyCustomer(uint256 _agreementId) {
+        require(agreements[_agreementId].customer == msg.sender, "Only the customer can perform this action");
         _;
     }
 
@@ -74,36 +89,55 @@ contract MasterContract {
     }
 
     function createAgreement(
-        address _supplier, 
         address _customer, 
-        uint256 _totalAmount, 
-        string[] memory _milestoneDescriptions,  
+        uint256 _totalAmount,  
         uint256 _deadline, 
-        uint256[] memory _milestoneAmounts, 
         string memory _terms
-        ) external onlyRegistered returns(uint256 aggreementId)  {
-        require(_supplier != address(0) && _customer != address(0), "Invalid addresses");
+        ) external returns(uint256 aggreementId)  {
+        require(_customer != address(0), "Invalid addresses");
 
         B2BAgreement storage newAggreement = agreements[aggreementCounter];
-        newAggreement.supplier = _supplier;
         newAggreement.customer = _customer;
         newAggreement.totalAmount = _totalAmount;
         newAggreement.deadline = _deadline;
         newAggreement.termsOfBusiness = _terms;
 
-          for (uint256 i = 0; i < _milestoneDescriptions.length; i++) {
-            newAggreement.milestones.push(Milestone({
-                description: _milestoneDescriptions[i],
-                amount: _milestoneAmounts[i],
-                status: mileStoneStatus.Pending // initial status is Pending
-            }));
-        }
-
-        emit AgreementCreated(aggreementCounter, _supplier, _customer, _totalAmount);
+        emit AgreementCreated(aggreementCounter, _customer, _totalAmount);
 
         return aggreementCounter++;
         
     }
+
+
+    function addSupplier(
+        uint256 _agreementId, 
+        address _supplier, 
+        string[] memory _milestoneDescriptions, 
+        uint256[] memory _milestoneAmounts
+    ) external onlyCustomer(_agreementId)
+    {
+        require(_supplier != address(0), "Invalid supplier address");
+        require(_milestoneDescriptions.length == _milestoneAmounts.length, "Descriptions and amounts length mismatch");
+
+        B2BAgreement storage agreement = agreements[_agreementId];
+        require(agreement.suppliers[_supplier].supplierAddresses == address(0), "Supplier already added");
+
+        //create supplier
+        Supplier storage supplier = agreement.suppliers[_supplier];
+        supplier.supplierAddresses = _supplier;
+
+        for(uint256 i =0; i < _milestoneDescriptions.length; i++){
+            supplier.milestones.push(Milestone({
+                description: _milestoneDescriptions[i],
+                amount: _milestoneAmounts[i],
+                status: mileStoneStatus.Pending
+            }));
+        }
+
+        agreement.supplierAddresses.push(_supplier);
+        emit SupplierAdded(_agreementId, _supplier);
+
+    } 
 
     function depositToEscrow(uint256 _aggreementId, uint256 amount) external {
         require(amount > 0, "Must send some funds");
@@ -114,64 +148,78 @@ contract MasterContract {
         emit DepositReceived(amount, msg.sender);
     }
 
+
     //supplier marks milestone completed
     function markMilesstoneCompleted(uint256 _agreementId, uint256 _milestoneIndex ) external {
-        B2BAgreement storage agreement = agreements[_agreementId];
-        require(msg.sender == agreement.supplier, "Only supplier can mark as complete");
-        require(agreement.milestones[_milestoneIndex].status == mileStoneStatus.Pending, "Milestone already completed");
 
-        agreement.milestones[_milestoneIndex].status = mileStoneStatus.Completed;
+        B2BAgreement storage agreement = agreements[_agreementId];
+        Supplier storage supplier = agreement.suppliers[msg.sender];
+
+        require(supplier.supplierAddresses == msg.sender, "Only supplier can mark as complete");
+        require(supplier.milestones[_milestoneIndex].status == mileStoneStatus.Pending, "Milestone already completed");
+
+        supplier.milestones[_milestoneIndex].status = mileStoneStatus.Completed;
         emit MilestoneCompleted(_agreementId, _milestoneIndex);
+
+        // Generate the invoice automatically
+        generateInvoice(_agreementId, _milestoneIndex, msg.sender);
 
     }
 
     // customer confirms the completion of milestone
-    function confirmMilestoneCompletion(uint256 _agreementId, uint256 _milestoneIndex) external {
-        B2BAgreement storage agreement = agreements[_agreementId];
-        require(msg.sender == agreement.customer, "Only customer can confirm");
+    function confirmMilestoneCompletion(uint256 _agreementId, address _supplier, uint256 _milestoneIndex) external {
 
-        agreement.milestones[_milestoneIndex].status = mileStoneStatus.customerConfirmed;
+        B2BAgreement storage agreement = agreements[_agreementId];
+        Supplier storage supplier = agreement.suppliers[msg.sender];
+        
+        require(supplier.milestones[_milestoneIndex].status == mileStoneStatus.Completed, "Milestone not completed by supplier");
+
+        supplier.milestones[_milestoneIndex].status = mileStoneStatus.customerConfirmed;
 
         // Release payment if both parties confirm the milestone
-        if (agreement.milestones[_milestoneIndex].status == mileStoneStatus.Completed && 
-            agreement.milestones[_milestoneIndex].status == mileStoneStatus.customerConfirmed) {
-            releasePayment(_agreementId, _milestoneIndex);
+        if (supplier.milestones[_milestoneIndex].status == mileStoneStatus.Completed && 
+            supplier.milestones[_milestoneIndex].status == mileStoneStatus.customerConfirmed) {
+            releasePayment(_agreementId, _supplier, _milestoneIndex);
         }
     }
 
     // Release payment for a completed milestone using ERC20 tokens
-    function releasePayment(uint256 _agreementId, uint256 _milestoneIndex) internal {
-        B2BAgreement storage agreement = agreements[_agreementId];
-        require(agreement.milestones[_milestoneIndex].status == mileStoneStatus.Completed, "Milestone not completed");
-        require(agreement.milestones[_milestoneIndex].status == mileStoneStatus.customerConfirmed, "Milestone not confirmed by customer");
-        require(agreement.milestones[_milestoneIndex].status == mileStoneStatus.supplierConfirmed, "Milestone not confirmed by supplier");
+    function releasePayment(uint256 _agreementId, address _supplier, uint256 _milestoneIndex) internal {
 
-        uint256 paymentAmount = agreement.milestones[_milestoneIndex].amount;
+        B2BAgreement storage agreement = agreements[_agreementId];
+        Supplier storage supplier = agreement.suppliers[msg.sender];
+
+        require(supplier.milestones[_milestoneIndex].status == mileStoneStatus.Completed, "Milestone not completed");
+        require(supplier.milestones[_milestoneIndex].status == mileStoneStatus.customerConfirmed, "Milestone not confirmed by customer");
+        require(supplier.milestones[_milestoneIndex].status == mileStoneStatus.supplierConfirmed, "Milestone not confirmed by supplier");
+
+        uint256 paymentAmount = supplier.milestones[_milestoneIndex].amount;
         
         // Transfer the amount from this contract to the supplier
-        require(paymentToken.transfer(agreement.supplier, paymentAmount), "Token transfer failed");
+        require(paymentToken.transfer(supplier.supplierAddresses, paymentAmount), "Token transfer failed");
         
-        agreement.milestones[_milestoneIndex].status = mileStoneStatus.Paid; // Update the milestone to paid status
+        supplier.milestones[_milestoneIndex].status = mileStoneStatus.Paid; // Update the milestone to paid status
 
-        emit PaymentReleased(_agreementId, _milestoneIndex, paymentAmount);
+        emit PaymentReleased(_agreementId, _supplier, _milestoneIndex, paymentAmount);
     }
 
     //invoice for a milestone
-    function generateInvoice(uint256 __aggreementId, uint256 _milestoneIndex) external {
-        B2BAgreement storage agreement = agreements[__aggreementId];
-        require(msg.sender == agreement.supplier || msg.sender == agreement.customer, "Unauthorized access");
+    function generateInvoice(uint256 _aggreementId, uint256 _milestoneIndex, address _supplier) internal {
+        
+        B2BAgreement storage agreement = agreements[_aggreementId];
+        Supplier storage supplier = agreement.suppliers[_supplier];
 
         string memory invoiceDetails = string(abi.encodePacked(
             "Invoice for milestone ", 
-            agreement.milestones[_milestoneIndex].description,
+            supplier.milestones[_milestoneIndex].description,
             ". Amount: ", 
-            uint2str(agreement.milestones[_milestoneIndex].amount)
+            uint2str(supplier.milestones[_milestoneIndex].amount)
         ));
 
-        emit InvoiceGenerated(__aggreementId, _milestoneIndex, invoiceDetails);
+        emit InvoiceGenerated(_aggreementId, _supplier, _milestoneIndex, invoiceDetails);
     }
 
-       // Helper function to convert uint to string
+    // Helper function to convert uint to string
     function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
         if (_i == 0) {
             return "0";
