36239a37aa18ef08c8da20b7bb77e30219cfd0af
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-10-15 16:12:12 +0300

    resolved conflicts

--- contracts/Multiparty.sol
+++ contracts/Multiparty.sol
@@ -1,217 +1,3 @@
-
-//SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.27;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-contract Multiparty {
-
-
-  address owner;
-  address tokenAddress;
-  uint  agreementID; 
-  uint[] public agreementIDs;
-  
-
-  enum Milestones {
-    STARTED,
-    PARTLY_READY,
-    COMPLETED
-  }
-
-
-  struct MultipartyRecord {
-    uint  _id;
-    address[] partyMembers;
-    uint[] amountAllocated;
-    string[] deliveryDate;
-    uint penalty;
-    mapping(address => uint256) totalDeposits;
-
-  }
-  
-  mapping(uint => mapping(address => MultipartyRecord)) public multipartyList;
-  mapping (uint => bool) isCreated;
-  
-
- 
-  address public multipartyCreator;
-  address public multipartyCreatorBalance;
-  address public erc20TokenAddress;
-
-  error ADDRESS_ZERO_NOT_PERMITED();
-  error NOT_AUTHORIZE_TO_CALL_THIS_FUNCTION();
-  error PARTY_MEMBERS_VALUE_CANNOT_BE_EMPTY();
-  error VALUE_CANNOT_BE_EMPTY();
-  error LENGTH_MUST_BE_SAME_WITH_PARTYMEMBERS_LENGTH();
-  error PENALTY_RATE_MUST_BE_SET();
-  error NOT_A_MEMBER();
-  error NOT_AN_ALLOCATED_AMOUNT();
-  error INSUFFICIENT_AMOUNT();
-
-  event MultiPartyCreatedSuccessfully(address indexed whoCreates , uint256 indexed agreementID);
-  event  DepositSuccessful(address indexed depositor , uint256 amount, uint256 agreementID );
-  event MultiPartyCreatedSuccessfully(address indexed whoCreates);
-
-
-
-  constructor(address _tokenAddress){
-    if(msg.sender == address(0)){
-      revert ADDRESS_ZERO_NOT_PERMITED();
-    }
-    owner = msg.sender;
-     tokenAddress = _tokenAddress;
-  }
-
-  function createMultiPartySystem(
-   
-    address[] memory _partMem, 
-    uint[] memory _amountAllocated,
-    string[] memory _deliveryDate,
-    uint _penalty
-    ) public {
-      if(msg.sender == address(0)){
-        revert ADDRESS_ZERO_NOT_PERMITED();
-      }
-      if(msg.sender != owner){
-        revert NOT_AUTHORIZE_TO_CALL_THIS_FUNCTION();
-      }
-      uint256 lengthOfArrayCheck = _partMem.length;
-
-      if(_partMem.length == 0){
-        revert PARTY_MEMBERS_VALUE_CANNOT_BE_EMPTY();
-      }
-
-      if(lengthOfArrayCheck != _amountAllocated.length ||
-      lengthOfArrayCheck != _deliveryDate.length){
-        
-        revert LENGTH_MUST_BE_SAME_WITH_PARTYMEMBERS_LENGTH();
-      }
-
-     
-
-      if(_penalty == 0){
-        revert PENALTY_RATE_MUST_BE_SET();
-      }
-
-      uint _id = agreementID + 1;
-
-      MultipartyRecord storage record = multipartyList[_id][msg.sender];
-
-      record._id = _id;
-      record.partyMembers =_partMem;
-      record.amountAllocated = _amountAllocated;
-      record.deliveryDate = _deliveryDate;
-      record.penalty = _penalty;
-
-      isCreated[_id] = true;
-
-      agreementID += 1;
-      agreementIDs.push(_id);
-
-      emit MultiPartyCreatedSuccessfully(msg.sender, _id);
-      
-  }
-
-
-
-
-  function depositToPlatform(uint256 _amount, uint256 _id) public {
-
-  require(msg.sender != address(0), "zero address");
-  require( isCreated[_id] , "invalid Agreement");
-
-  MultipartyRecord storage record = multipartyList[_id][msg.sender];
-
-    bool isMember = false;
-    uint256 allocatedAmount;
-}
-  // function for the creator to send token to the contract
-  // function depositToken(uint256 _amount) external{
-  //   if(msg.sender == address(0)){
-  //     revert ADDRESS_ZERO_NOT_PERMITED();
-  //   }
-  //   if(erc20TokenAddress == address(0)){
-  //     revert ADDRESS_ZERO_NOT_PERMITED();
-  //   }
-  //   IERC20(erc20TokenAddress).transferFrom(multipartyCreator, address(this), _amount);
-  // }
-
-/*Release Payment*/
-function releasePayment(uint256 partyMemberId, uint256 milestoneIndex, uint256 milestone1Payment, uint256 milestone2Payment, uint256 milestone3Payment) external {
-    // We will ensure that the milestone is completed before releasing payment
-    // We assume that there is a mapping to track milestone completion for each party member
-    
-   
-    for (uint256 i = 0; i < record.partyMembers.length; i++) {
-        if (record.partyMembers[i] == msg.sender) {
-            isMember = true;
-            allocatedAmount = record.amountAllocated[i]; 
-            break;
-        }
-    }
-
-    if (!isMember) {
-        revert NOT_A_MEMBER();
-    }
-
-    if (_amount != allocatedAmount) {
-        revert NOT_AN_ALLOCATED_AMOUNT();
-    }
-
-    IERC20 token = IERC20(tokenAddress); 
-    require(token.allowance(msg.sender, address(this)) >= _amount, "Allowance not sufficient");
-    uint256 _userTokenBalance = token.balanceOf(msg.sender);
-
-     if (_userTokenBalance < _amount) {
-
-        revert INSUFFICIENT_AMOUNT();
-    }
-   
-
-    token.transferFrom(msg.sender, address(this), _amount);
-
-    record.totalDeposits[msg.sender] += _amount;
-
-    emit  DepositSuccessful(msg.sender, _amount, _id);
-}
-
-
-function getAllAgreementIDs() public view returns (uint[] memory) {
-    return agreementIDs;
-}
-
-function getAgreementDetails(uint _id) 
-    public view returns (address[] memory, uint[] memory,string[] memory, uint) 
-{
-    MultipartyRecord storage record = multipartyList[_id][msg.sender];
-
-    return (record.partyMembers, record.amountAllocated, record.deliveryDate, record.penalty);
-}
-
-
-// function releasePayment( Milestones _mileStone,uint _amount, uint _id) external {
-//    MultipartyRecord storage record = multipartyList[_id][msg.sender];
-
-//         if(_mileStone == Milestones.STARTED){
-//             record.penalty;
-            
-//         }else if (_mileStone == Milestones.PARTLY_READY){
-//           record.penalty ;
-             
-//         } else {
-//          record.penalty;
-          
-//         }
-
-// }
-
-
-
-
-}
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
@@ -280,7 +66,7 @@ contract PayNVoice {
     function addMilestone(
         uint256 _invoiceId,
         string memory _description,
-        uint256 _amount
+        uint256 _amount,
         uint256 _deadline
     ) public {
         Invoice storage invoice = invoices[invoiceCreator][_invoiceId];
@@ -288,7 +74,7 @@ contract PayNVoice {
             description: _description,
             amount: _amount,
             status: Status.pending,
-            isPaid: false
+            isPaid: false,
             deadline: _deadline
         }));
         emit MilestoneAdded(_invoiceId, _description, _amount);
@@ -465,81 +251,65 @@ contract PayNVoice {
         }
 
         return clientInvoices;
-}
+    }
+
 
 
 // the person who deposited into our escrow is doing this
-function confirmPaymentRelease(uint256 invoiceId) public {
-    if(msg.sender == address(0)){
-        revert ADDRESS_ZERO_NOT_PERMITED();
-    }
-
-    Invoice storage invoice = invoices[invoiceCreator][invoiceId];
-
-    if(msg.sender != invoice.clientAddress){
-        revert NOT_AUTHORIZE_TO_CALL_THIS_FUNCTION();
-    }
-    if(invoice.amount == 0){
-        revert CANT_INITIATE_RELEASE();
-    }
-
-    uint256 milestoneLength = invoice.milestones.length;
-
-    for(uint256 counter = 0; counter < milestoneLength; counter++){
-        if(invoice.milestones[counter].isPaid == false){
-
-            //here is when both parties have fulfilled what was in agreement
-            if(block.timestamp <= invoice.milestones[counter].deadline && block.timestamp <= invoice.deadline && invoice.milestones[counter].status == Status.confirmed){
-                invoice.milestones[counter].isPaid == true;
-                IERC20(erc20TokenAddress).transferFrom(address(this), invoice.clientAddress, invoice.milestones[counter].amount);
-                break;
-            } 
-
-            //Here is when client pays past the deadline of Invoice issuance
-
-            if(block.timestamp <= invoice.milestones[counter].deadline && block.timestamp > invoice.deadline && invoice.milestones[counter].status == Status.confirmed){
-
-              uint256 lateFee = calculateLateFee(invoiceId);
-              uint256 amountToBeRealeased = invoice.amount + lateFee;
-
-              require(userTokenBal >= amountToBeRealeased, "Insufficient balance for amount and late fee");
-              invoice.isPaid = true;
-
-              IERC20(erc20TokenAddress).transferFrom(address(this), invoice.clientAddress, amountToBeRealeased);
-            }
-
-            //Here is when the Supplier delivers past the milestone deadline
-
-            if(block.timestamp > invoice.milestones[counter].deadline && invoice.milestones[counter].status == Status.confirmed){
-                lateFee =  calculateLateFeeForSupplier(invoiceId, invoice.milestones[counter])
-                uint256 amountToBeRealeased = invoice.milestones[counter].amount - lateFee;
-
-                invoice.milestones[counter].isPaid == true;
-                IERC20(erc20TokenAddress).transfer(address(this), invoice.clientAddress, amountToBeRealeased);
-                break;
-            } 
+    function confirmPaymentRelease(uint256 invoiceId) public {
+        if(msg.sender == address(0)){
+            revert ADDRESS_ZERO_NOT_PERMITED();
         }
+
+        Invoice storage invoice = invoices[invoiceCreator][invoiceId];
+
+        if(msg.sender != invoice.clientAddress){
+            revert NOT_AUTHORIZE_TO_CALL_THIS_FUNCTION();
+        }
+        if(invoice.amount == 0){
+            revert CANT_INITIATE_RELEASE();
+        }
+
+        uint256 milestoneLength = invoice.milestones.length;
+        uint256 userTokenBal = IERC20(erc20TokenAddress).balanceOf(msg.sender);
+
+
+        for(uint256 counter = 0; counter < milestoneLength; counter++){
+            if(invoice.milestones[counter].isPaid == false){
+
+                //here is when both parties have fulfilled what was in agreement
+                if(block.timestamp <= invoice.milestones[counter].deadline && block.timestamp <= invoice.deadline && invoice.milestones[counter].status == Status.confirmed){
+                    invoice.milestones[counter].isPaid == true;
+                    IERC20(erc20TokenAddress).transferFrom(address(this), invoice.clientAddress, invoice.milestones[counter].amount);
+                    break;
+                } 
+
+                //Here is when client pays past the deadline of Invoice issuance
+
+                if(block.timestamp <= invoice.milestones[counter].deadline && block.timestamp > invoice.deadline && invoice.milestones[counter].status == Status.confirmed){
+
+                uint256 lateFee = calculateLateFee(invoiceId);
+                uint256 amountToBeRealeased = invoice.amount + lateFee;
+
+                require(userTokenBal >= amountToBeRealeased, "Insufficient balance for amount and late fee");
+                invoice.isPaid = true;
+
+                IERC20(erc20TokenAddress).transferFrom(address(this), invoice.clientAddress, amountToBeRealeased);
+                }
+
+                //Here is when the Supplier delivers past the milestone deadline
+
+                if(block.timestamp > invoice.milestones[counter].deadline && invoice.milestones[counter].status == Status.confirmed){
+                    uint256 lateFee =  calculateLateFeeForSupplier(invoiceId, counter);
+                    uint256 amountToBeRealeased = invoice.milestones[counter].amount - lateFee;
+
+                    invoice.milestones[counter].isPaid == true;
+                    IERC20(erc20TokenAddress).transferFrom(address(this), invoice.clientAddress, amountToBeRealeased);
+                    break;
+                } 
+            }
+        }
+
+    
     }
-
-    // if(invoice.isPaid == true){
-    //     revert PAYMENT_HAS_BEEN_MADE();
-    // }
-
-    // delete invoices[invoiceCreator][invoiceId];
 }
-
-// function requestForPaymentRelease(uint256 invoiceId, uint256 milestone) external{
-//     if(msg.sender == address(0)){
-//         revert ADDRESS_ZERO_NOT_PERMITTED();
-//     }
-
-//     if(msg.sender )
-//     invoices[invoiceCreator][invoiceId];
-//     if()
-// }
-
-
-}
-    // Add additional functions for payment release and other operations as needed
-}
-
