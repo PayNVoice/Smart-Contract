3d6242e6af80289ab1402ccaa3feb0e858eed365
olawalesegun <olawalesegun0@gmail.com>
2024-10-15 04:30:27 +0300

    fixxes in invoice factory and additional implementation

--- contracts/invoiceFactory.sol
+++ contracts/invoiceFactory.sol
@@ -1,30 +1,30 @@
 
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
-// import "./InvoiceContract.sol";
+import "./InvoiceContract.sol";
 
-// contract PayNVoiceFactory {
+contract PayNVoiceFactory {
 
-//     PayNVoice[] invoiceClones;
+    PayNVoice[] invoiceClones;
 
-//     function createInvoice() external returns (PayNVoice newInvoice_, uint256 length_) {
+    function createInvoice(address tokAddress) external returns (PayNVoice newInvoice_, uint256 length_) {
 
-//         newInvoice_ = new PayNVoice();
+        newInvoice_ = new PayNVoice(tokAddress);
 
-//         invoiceClones.push(newInvoice_);
+        invoiceClones.push(newInvoice_);
 
-//         length_ = invoiceClones.length;
-//     }
+        length_ = invoiceClones.length;
+    }
 
-//     function getInvoiceClones() external view returns(PayNVoice[] memory) {
-//         return invoiceClones;
-//     }
+    function getInvoiceClones() external view returns(PayNVoice[] memory) {
+        return invoiceClones;
+    }
 
-//      function getANInvoiceClone(uint _index) external view returns (PayNVoice) {
-//         return invoiceClones[_index];
-//     }
-// }
+     function getANInvoiceClone(uint _index) external view returns (PayNVoice) {
+        return invoiceClones[_index];
+    }
+}
 
 
 
